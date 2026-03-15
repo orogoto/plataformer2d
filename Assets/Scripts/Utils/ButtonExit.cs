c7087e3b0fbade5cf3f7bc1f14ccbca27bfc5e60
orogoto <carlosbreder.11@gmail.com>
2026-03-15 09:03:03 -0300

    agr foi

--- Assets/Scripts/Utils/ButtonExit.cs
+++ Assets/Scripts/Utils/ButtonExit.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ButtonExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Quit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
