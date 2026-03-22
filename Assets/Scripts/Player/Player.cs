6e849544f7400b4dc99f26bd292bbe65883477dc
orogoto <carlosbreder.11@gmail.com>
2026-03-22 09:16:25 -0300

    more movenent

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,15 +6,19 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myrigibody;
     public Vector2 velocity;
+    public float speed;
     private void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myrigibody.MovePosition(myrigibody.position + velocity * Time.deltaTime);        }
+            //myrigibody.MovePosition(myrigibody.position - velocity * Time.deltaTime);
+            myrigibody.velocity = new Vector2(-speed, myrigibody.velocity.y);
+        }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myrigibody.MovePosition(myrigibody.position - velocity * Time.deltaTime);
+            //myrigibody.MovePosition(myrigibody.position + velocity * Time.deltaTime);
+            myrigibody.velocity = new Vector2(speed, myrigibody.velocity.y);
         }
     }
 }
