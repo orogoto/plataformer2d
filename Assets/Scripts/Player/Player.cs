5ce366f2d5687805cfc5cb2e664826b9b11ea68b
orogoto <carlosbreder.11@gmail.com>
2026-03-22 08:42:45 -0300

    add moviment to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D myrigibody;
+    public Vector2 velocity;
+    private void Update()
     {
-        
-    }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myrigibody.MovePosition(myrigibody.position + velocity * Time.deltaTime);        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myrigibody.MovePosition(myrigibody.position - velocity * Time.deltaTime);
+        }
     }
 }
