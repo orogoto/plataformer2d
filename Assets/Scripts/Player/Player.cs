8c1e04bb0856a25d684e04dc4718946bbe363cd5
orogoto <carlosbreder.11@gmail.com>
2026-04-02 08:21:25 -0300

    add jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myrigibody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float jumpforce = 2;
     private void Update()
+    {
+        handlejump();
+        handlemoviment();
+    }
+
+    private void handlemoviment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -20,5 +27,26 @@ public class Player : MonoBehaviour
             //myrigibody.MovePosition(myrigibody.position + velocity * Time.deltaTime);
             myrigibody.velocity = new Vector2(speed, myrigibody.velocity.y);
         }
+
+        if (myrigibody.velocity.x > 0)
+        {
+            myrigibody.velocity += friction;
+        }
+
+        else if (myrigibody.velocity.x < 0)
+        {
+            myrigibody.velocity -= friction;
+        }
+
     }
+
+
+    private void handlejump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            myrigibody.velocity = Vector2.up * jumpforce;
+        }
+    }
+
 }
