60294fbb7cfbe62527ebfba2840ec101e6b06d70
orogoto <carlosbreder.11@gmail.com>
2026-04-03 08:28:25 -0300

    add jump animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class Player : MonoBehaviour
     public float speedrun;
     public float jumpforce = 2;
     private float _currentspeed;
+    private float jumpscaley = 1.5f;
+    private float jumpscalex = 1.5f;
+    public float animationDuration = .3f;
     private void Update()
     {
         handlejump();
@@ -53,7 +57,15 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         { 
             myrigibody.velocity = Vector2.up * jumpforce;
+            myrigibody.transform.localScale = Vector2.one;
+            DOTween.Kill(myrigibody.transform);
+            Handlescalejump();
         }
     }
 
+    private void Handlescalejump()
+    {
+        myrigibody.transform.DOScaleY(jumpscaley, animationDuration).SetLoops(2, LoopType.Yoyo);
+        myrigibody.transform.DOScaleX(jumpscalex, animationDuration).SetLoops(2, LoopType.Yoyo);
+    }
 }
