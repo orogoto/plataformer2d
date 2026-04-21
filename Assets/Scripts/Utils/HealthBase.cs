bfee5cd0bf933ed72521f85d3cb9f6d950bc1298
orogoto <carlosbreder.11@gmail.com>
2026-04-21 10:28:31 -0300

    add Life and a enemy

---
+++ Assets/Scripts/Utils/HealthBase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    public int startlife = 10;
+
+    private int _currentlife;
+
+    private bool _isDead = false;
+
+    public  bool DestroyOnKill = false;
+
+    public void Awake()
+    {
+        init();
+       
+    }
+
+    private void init()
+    {
+        _isDead = false;
+        _currentlife = startlife;
+    }
+
+    public void Damage(int damage)
+    {
+        if (_isDead) return;
+        if (_currentlife <= 0)
+        {
+            Kill();
+        }
+    }
+
+    private void Kill()
+    {
+        _isDead=true;
+        if (DestroyOnKill)
+        {
+            Destroy (gameObject);
+        }
+    }
+}
